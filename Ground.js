3a9d6f804290b015e9f2a10e7046b94a47e8091a
Moazzam123-coder <67272536+Moazzam123-coder@users.noreply.github.com>
2020-07-24 17:49:22 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,18 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.color="brown";
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill(this.color);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
